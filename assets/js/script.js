9fbd2df1b62e7620303a83fb13075edaef48ef30
Hilary Richardson <hilary.richardson@gmail.com>
2021-01-18 11:49:29 -0800

    comment readability additions for script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,8 +179,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
-// add sortable to card lists
+// enable draggable/sortable feature on list-group elements
 $(".card .list-group").sortable({
+  // enable dragging across lists
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
@@ -201,24 +202,22 @@ $(".card .list-group").sortable({
     // array to store the task data in
     var tempArr = [];
     // loop over current set of children in sortable list
-    $(this).children().each(function () {
-      var text = $(this)
-        .find("p")
-        .text()
-        .trim();
-
-      var date = $(this)
-        .find("span")
-        .text()
-        .trim();
-
-      // add task data to the temp array as an object
-      tempArr.push({
-        text: text,
-        date: date
+    $(this)
+      .children()
+      .each(function () {
+        // save values in temp array
+        tempArr.push({
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+        });
       });
-    });
-    console.log(tempArr);
+
     // trim down list's ID to match object property
     var arrName = $(this)
       .attr("id")
@@ -227,14 +226,19 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
+  },
+  stop: function (event) {
+    $(this).removeClass("dropover");
   }
 });
 
+// trash icon can be dropped onto
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
     console.log("drop");
+    // remove dragged element from the dom
     ui.draggable.remove();
   },
   over: function (event, ui) {
